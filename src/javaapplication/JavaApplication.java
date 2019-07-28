c33ae440f879a8aa954b49ebbb6432cb2ee4c7e1
mh <objectifmh@hotmail.com>
2019-07-28 17:53:44 +0200

    factoriser les fonctions

--- src/javaapplication/JavaApplication.java
+++ src/javaapplication/JavaApplication.java
@@ -6,6 +6,7 @@
 package javaapplication;
 
 import java.util.Scanner;
+import static javaapplication.JavaApplication.tab;
 
 /**
  *
@@ -15,70 +16,90 @@ public class JavaApplication {
 
     public static boolean isInc = true;
 
+    public static int _debOrd = 0;
+    public static int _debAbs = 0;
+
+    public static int _cpt = 1;
+    public static int tab[][] = null;
+
+    public static int _abs = 0;
+    public static int _limitAbs;
+
+    public static int _ord = 0;
+    public static int _limitOrd;
+
+    public static int debOrd() {
+        while (_abs < _limitAbs) {
+            tab[_abs][_ord] = _cpt;
+            _cpt++;
+            _abs++;
+        }
+        return _debOrd++;
+        //return 
+
+    }
+
+    public static int limitAbs() {
+        while (_ord < _limitOrd) {
+            tab[_abs][_ord] = _cpt;
+            _cpt++;
+            _ord++;
+        }
+        return _limitAbs--;
+    }
+
+    public static int limitOrd() {
+        while (_abs > _debAbs) {
+            tab[_abs][_ord] = _cpt;
+            _cpt++;
+            _abs--;
+        }
+        return _limitOrd--;
+    }
+
+    public static int debAbs() {
+        while (_ord > _debOrd) {
+            tab[_abs][_ord] = _cpt;
+            _cpt++;
+            _ord--;
+        }
+        return _debAbs++;
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        System.out.println("Colimasson : ");
+        System.out.println("Colimaçon : ");
         System.out.println("Quelle est la taille de votre tableau ?");
 
         Scanner sc = new Scanner(System.in);
         int taille = sc.nextInt();
         int tailleTotale = taille * taille;
-        int cpt = 1;
+        
+        tab = new int[taille][taille];
 
-        int abs = 0;
-        int cord = 0;
-        int tab[][] = new int[taille][taille];
+        _limitAbs = taille - 1;
+        _limitOrd = taille - 1;
 
-        int limitAbs = taille - 1;
-        int limitOrd = taille - 1;
-        int debAbs = 0;
-        int debOrd = 0;
-
-        while (cpt < tailleTotale  ) {
+        while (_cpt < tailleTotale) {
             if (isInc == true) {
-                while (abs < limitAbs) {
-                    tab[abs][cord] = cpt;
-                    System.out.println("-> " + abs + "," + cord + " : " + tab[abs][cord] + " > "+limitAbs);
-                    cpt++;
-                    abs++;
-                    
-                }
-                debOrd++;
-                while (cord < limitOrd) {
-                    tab[abs][cord] = cpt;
-                    System.out.println("v " + abs + "," + cord + " : " + tab[abs][cord] + " > "+limitOrd);
-                    cpt++;
-                    cord++;
-                }
-                limitAbs--;
+                debOrd();
+                limitAbs();
                 isInc = false;
             }
             if (isInc == false) {
-                while (abs > debAbs) {
-                    tab[abs][cord] = cpt;
-                    System.out.println("<- " + abs + "," + cord + " : " + tab[abs][cord] + " > "+debAbs);
-                    cpt++;
-                    abs--;
-                }
-                limitOrd--;
-                while (cord > debOrd) {
-                    tab[abs][cord] = cpt;
-                    System.out.println("^ " + abs + "," + cord + " : " + tab[abs][cord] + " > "+ debOrd);
-                    cpt++;
-                    cord--;
-                }
-                debAbs++;
+                limitOrd();
+                debAbs();
                 isInc = true;
             }
 
         }
-        tab[abs][cord] = cpt ; 
+        tab[_abs][_ord] = _cpt;
 
         System.out.println("Mon Colimaçon");
-        for (int i = 0; i < taille ; i++) {
+        for (int i = 0; i < taille; i++) {
             for (int j = 0; j < taille; j++) {
                 System.out.print(tab[j][i] + "\t");
             }
